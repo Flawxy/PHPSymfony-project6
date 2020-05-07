156356a0530f085864e5dcffe86c740d43b87ff3
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-05-07 11:43:25 +0200

    Creating forms & updating controller
    🟢 Creating MediaType ;
    🟢 Creating TrickType ;
    🟠 Updating TrickController.

---
+++ src/Form/TrickType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Trick;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrickType extends AbstractType
+{
+    /**
+     * Allows to configure a form's field
+     *
+     * @param string $label
+     * @param string $placeholder
+     * @param array $options
+     * @return array
+     */
+    private function getConfiguration(string $label, string $placeholder, $options = [])
+    {
+        return array_merge([
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ], $options);
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class,
+                $this->getConfiguration('Nom', 'Indiquez le nom de la figure'))
+            ->add('description', TextareaType::class,
+                $this->getConfiguration('Description', 'Indiquez la description de la figure'))
+            ->add('coverImage', TextType::class,
+                $this->getConfiguration('Image principale', "Indiquez l'image de couverture"))
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'label' => 'Catégorie'
+            ])
+            ->add('medias', CollectionType::class, [
+                'entry_type' => MediaType::class,
+                'allow_add' => true
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Trick::class,
+        ]);
+    }
+}
