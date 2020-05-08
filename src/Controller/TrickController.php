c6bf9d8e5052c6be4c378a5918199e5e9fc45f0f
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-05-08 12:03:12 +0200

    Updating TrickController

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -35,4 +35,59 @@ class TrickController extends AbstractController
             'trick' => $trick
         ]);
     }
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
+
+    /**
+     * Displays the editing form of a trick
+     *
+     * @Route("/tricks/{slug}/edit", name="tricks_edit")
+     * @param Request $request
+     * @param Trick $trick
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function edit(Request $request, Trick $trick, EntityManagerInterface $manager)
+    {
+        $form = $this->createForm(TrickType::class, $trick);
+
+        $form->handleRequest($request);
+
+<<<<<<< Updated upstream
+        if($form->isSubmitted() && $form->isValid()) {
+=======
+        if ($form->isSubmitted() && $form->isValid()) {
+>>>>>>> Stashed changes
+            foreach ($trick->getMedias() as $media) {
+                $media->setTrick($trick);
+                $manager->persist($media);
+            }
+            $manager->persist($trick);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Les modifications de la figure <strong>{$trick->getName()}</strong> ont bien été enregistrées !"
+            );
+
+            return $this->redirectToRoute('tricks_show', [
+                'slug' => $trick->getSlug()
+            ]);
+        }
+
+        return $this->render('trick/edit.html.twig', [
+            'form' => $form->createView(),
+            'trick' => $trick
+        ]);
+    }
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 }
