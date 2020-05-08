b8e091e071ed0e01f099816d514aadfec4d1f31e
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-05-08 12:01:33 +0200

    Updatin controller & creating form
    🟢 Creating PasswordUpdateType ;
    🟠 Updating AccountController ;
    🟠 Updating TrickController ;
    🟠 Updating RegistrationType.

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,23 +2,68 @@
 
 namespace App\Controller;
 
+<<<<<<< Updated upstream
 use App\Entity\User;
 use App\Form\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+=======
+use App\Entity\PasswordUpdate;
+use App\Entity\User;
+use App\Form\PasswordUpdateType;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+>>>>>>> Stashed changes
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
 {
     /**
      * Displays the register form
+<<<<<<< Updated upstream
      */
     public function register()
+=======
+     *
+     * @Route("/register", name="account_register")
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param UserPasswordEncoderInterface $encoder
+     * @return Response
+     */
+    public function register(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
+>>>>>>> Stashed changes
     {
         $user = new User();
         $form = $this->createForm(RegistrationType::class, $user);
 
+<<<<<<< Updated upstream
+=======
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $hashedPassword = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hashedPassword);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Votre compte a bien été créé ! Vous pouvez maintenant vous connecter !"
+            );
+
+            return $this->redirectToRoute('account_login');
+        }
+
+>>>>>>> Stashed changes
         return $this->render('account/registration.html.twig', [
             'form' => $form->createView()
         ]);
@@ -52,4 +97,52 @@ class AccountController extends AbstractController
     {
         // Managed by Symfony
     }
+<<<<<<< Updated upstream
+=======
+
+    /**
+     * Allows the user to change his password
+     *
+     * @Route("/account/update-password", name="account_password")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $encoder
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function updatePassword(Request $request, UserPasswordEncoderInterface $encoder, EntityManagerInterface $manager)
+    {
+        $passwordUpdate = new PasswordUpdate();
+        $user = $this->getUser();
+
+        $form = $this->createForm(PasswordUpdateType::class, $passwordUpdate);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if(!$encoder->isPasswordValid($user, $passwordUpdate->getOldPassword())) {
+                $form->get('oldPassword')->addError(new FormError("Le mot de passe renseigné est incorrect !"));
+            }else {
+
+                $newPassword = $passwordUpdate->getNewPassword();
+                $hashedPassword = $encoder->encodePassword($user, $newPassword);
+
+                $user->setPassword($hashedPassword);
+
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash(
+                    'success',
+                    "Votre mot de passe a bien été modifié !"
+                );
+
+                return $this->redirectToRoute('homepage');
+            }
+        }
+
+        return $this->render('account/password.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+>>>>>>> Stashed changes
 }
--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\User;
+<<<<<<< Updated upstream
 use Symfony\Component\Form\AbstractType;
+=======
+>>>>>>> Stashed changes
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -16,11 +19,21 @@ class RegistrationType extends ApplicationType
     {
         $builder
             ->add('nickname', TextType::class,
+<<<<<<< Updated upstream
                 $this->getConfiguration('Pseudo', 'Votre pseudonyme...'))
             ->add('mail', EmailType::class,
                 $this->getConfiguration('Email', 'Votre adresse email...'))
             ->add('password', PasswordType::class,
                 $this->getConfiguration('Mot de passe', 'Votre mot de passe...'))
+=======
+                $this->getConfiguration("Pseudo", "Votre pseudonyme..."))
+            ->add('mail', EmailType::class,
+                $this->getConfiguration("Email", "Votre adresse email..."))
+            ->add('password', PasswordType::class,
+                $this->getConfiguration("Mot de passe", "Votre mot de passe..."))
+            ->add('passwordConfirmation', PasswordType::class,
+                $this->getConfiguration("Confirmation du mot de passe", "Veuillez confirmer votre mot de passe..."))
+>>>>>>> Stashed changes
         ;
     }
 
